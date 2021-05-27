7b586f5b5170c931753e88dae29d7b8ea16cbea0
MikhaZyu <MikhaZyu@gmail.com>
2021-05-27 14:58:35 +0300

    Copy base project

---
+++ Otus.Teaching.Linq.ATM.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Otus.Teaching.Linq.ATM.Core.Services;
+using Otus.Teaching.Linq.ATM.DataAccess;
+
+namespace Otus.Teaching.Linq.ATM.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Старт приложения-банкомата...");
+
+            var atmManager = CreateATMManager();
+            
+            //TODO: Далее выводим результаты разработанных LINQ запросов
+            
+            System.Console.WriteLine("Завершение работы приложения-банкомата...");
+        }
+
+        static ATMManager CreateATMManager()
+        {
+            using var dataContext = new ATMDataContext();
+            var users = dataContext.Users.ToList();
+            var accounts = dataContext.Accounts.ToList();
+            var history = dataContext.History.ToList();
+                
+            return new ATMManager(accounts, users, history);
+        }
+    }
+}
---
+++ Otus.Teaching.Linq.ATM.Core/Services/ATMManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Otus.Teaching.Linq.ATM.Core.Entities;
+
+namespace Otus.Teaching.Linq.ATM.Core.Services
+{
+    public class ATMManager
+    {
+        public IEnumerable<Account> Accounts { get; private set; }
+        
+        public IEnumerable<User> Users { get; private set; }
+        
+        public IEnumerable<OperationsHistory> History { get; private set; }
+        
+        public ATMManager(IEnumerable<Account> accounts, IEnumerable<User> users, IEnumerable<OperationsHistory> history)
+        {
+            Accounts = accounts;
+            Users = users;
+            History = history;
+        }
+        
+        //TODO: Добавить методы получения данных для банкомата
+    }
+}
